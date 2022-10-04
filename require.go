e6f5653755b027f9b5ef28bddb10d0c8f84aebb0
Djalma Freire de Oliveira <djalma.oliveira@intecomlogistica.com.br>
2022-10-04 17:06:22 -0300

    :rocket: Require list

---
+++ require.go
@@ -0,0 +1,7 @@
+package calculo
+
+import (
+	_ "github.com/DjalmaFO/calculo_distancia_entre_pontos_golang/funcs"
+	_ "github.com/DjalmaFO/calculo_distancia_entre_pontos_golang/models"
+	_ "github.com/DjalmaFO/calculo_distancia_entre_pontos_golang/validator"
+)
