72eda9b78a3a3d3c2163eeffd2ea1af7b38000ab
Djalma Freire de Oliveira <djalma.oliveira@intecomlogistica.com.br>
2022-10-07 15:39:45 -0300

    :bug: Utilizando as coordenadas fornecidas

--- calculo_distancia_entre_pontos_golang.go
+++ calculo_distancia_entre_pontos_golang.go
@@ -6,8 +6,8 @@ import "github.com/DjalmaFO/calculo_distancia_entre_pontos_golang/pontos"
 // O calculo é realizado com base na latitude e longitude dos pontos 1 e 2.
 // A distancia é dada em Km (Kilometers)
 func CalcularDistancia(latitude1, longitude1, latitude2, longitude2 float64) (distancia float64, err error) {
-	ponto1 := pontos.Ponto{Latitude: 34.0522, Longitude: -118.2436}
-	ponto2 := pontos.Ponto{Latitude: 35.6850, Longitude: 139.7514}
+	ponto1 := pontos.Ponto{Latitude: latitude1, Longitude: longitude1}
+	ponto2 := pontos.Ponto{Latitude: latitude2, Longitude: longitude2}
 
 	return pontos.KmDistancia(ponto1, ponto2)
 }
